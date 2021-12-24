0b615a6f83e876dd9722b71d1efb5e5de717a2c2
Jesse <2302541+Kangaroux@users.noreply.github.com>
2021-12-24 17:56:05 -0500

    Update HTML, fix encoding issue

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/url"
 
 	"github.com/kangaroux/webview/api"
 	"github.com/webview/webview"
@@ -36,19 +37,21 @@ func main() {
 	defer w.Destroy()
 	w.SetTitle("Minimal webview example")
 	w.SetSize(800, 600, webview.HintNone)
-	w.Navigate(fmt.Sprintf(`data:text/html,
-<html>
-	<head>
-		<link rel="stylesheet" href="http://%[1]s/static/style.css">
-	</head>
-	<body>
-		<div id="root"></div>
-		<script type="text/javascript">
-			window.API_HOST = "http://%[1]s";
-		</script>
-		<script src="http://%[1]s/static/app.js"></script>
-	</body>
-</html>
-	`, addr))
+	data := fmt.Sprintf(`
+	<html>
+		<head>
+			<link rel="stylesheet" href="http://%[1]s/static/style.css">
+		</head>
+		<body>
+			<div id="root"></div>
+			<script type="text/javascript">
+				var API_HOST = "%[1]s";
+				var API_URL = "http://" + API_HOST;
+			</script>
+			<script src="http://%[1]s/static/app.js"></script>
+		</body>
+	</html>
+		`, addr)
+	w.Navigate("data:text/html," + url.QueryEscape(data))
 	w.Run()
 }
--- static/app.js
+++ static/app.js
@@ -1,2 +1,2 @@
 document.write("<h1>Hello world</h1>");
-document.write(`<p>API server is running on ${window.API_HOST}</p>`);
+document.write(`<p>API server is running on ${API_HOST}</p>`);
